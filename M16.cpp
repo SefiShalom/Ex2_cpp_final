ae2159ab2c3ff9422466ba75d6965c174d9e5d50
Sefi <Sefishalom@gmail.com>
2017-12-23 18:54:29 +0200

    Fixed segmentation fault

---
+++ M16.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sefi on 20/12/2017.
+//
+
+#include "M16.h"
---
+++ M16.h
@@ -0,0 +1,14 @@
+//
+// Created by Sefi on 20/12/2017.
+//
+
+#ifndef EX2_CPP_SEFI_M16_H
+#define EX2_CPP_SEFI_M16_H
+
+
+class M16 {
+
+};
+
+
+#endif //EX2_CPP_SEFI_M16_H
